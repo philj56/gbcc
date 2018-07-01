9118c655be03f7093591817be35cebfcd2e60825
Phil Jones <philj56@gmail.com>
2018-07-01 11:55:42 +0100

    Committing nearly working audio before rewriting it

--- src/gbcc_audio.c
+++ src/gbcc_audio.c
@@ -1,30 +1,42 @@
 #include "gbcc.h"
 #include "gbcc_audio.h"
+#include "gbcc_bit_utils.h"
 #include "gbcc_debug.h"
 #include "gbcc_memory.h"
 #include <SDL2/SDL.h>
 #include <stdint.h>
 #include <time.h>
 
+// TODO: add single samples at a time, as needed.
+
 /* 4 channels, with volume 0x00-0x0F each */
 #define BASE_AMPLITUDE (INT16_MAX / 4 / 0x0F)
 #define SAMPLE_RATE 48000
-#define BUFSIZE SAMPLE_RATE
-#define SYNC_PERIOD 1e6 /* nanoseconds */
+#define SAMPLE_CLOCKS (GBC_CLOCK_FREQ / SAMPLE_RATE)
+#define BUFSIZE 1024
+#define SYNC_PERIOD (1e9 / 256) /* nanoseconds */
 #define SYNC_CLOCKS (SYNC_PERIOD / GBC_CLOCK_PERIOD)
+#define LAG 1e7 /* nanoseconds */
+
+struct channel {
+	Sint16 buffer[BUFSIZE];
+	uint64_t sample;
+	uint64_t counter;
+};
 
 struct gbcc_audio {
 	SDL_AudioSpec audiospec;
 	SDL_AudioDeviceID device;
 	struct timespec cur_time;
 	struct timespec old_time;
+	struct timespec rem_time;
 	uint64_t cur_clock;
 	uint64_t old_clock;
 	Sint16 mix_buffer[BUFSIZE];
-	Sint16 ch1_buffer[BUFSIZE]; 	/* Tone & Sweep */
-	Sint16 ch2_buffer[BUFSIZE]; 	/* Tone */
-	Sint16 ch3_buffer[BUFSIZE]; 	/* Wave Output */
-	Sint16 ch4_buffer[BUFSIZE]; 	/* Noise */
+	struct channel ch1; 	/* Tone & Sweep */
+	struct channel ch2; 	/* Tone */
+	struct channel ch3; 	/* Wave Output */
+	struct channel ch4; 	/* Noise */
 };
 
 static struct gbcc_audio gbcc_audio;
@@ -53,6 +65,10 @@ void gbcc_audio_initialise(void)
 	timespec_get(&gbcc_audio.cur_time, TIME_UTC);
 	gbcc_audio.old_clock = 0;
 	gbcc_audio.cur_clock = 0;
+	gbcc_audio.ch1.sample = 0;
+	gbcc_audio.ch2.sample = 0;
+	gbcc_audio.ch3.sample = 0;
+	gbcc_audio.ch4.sample = 0;
 
 	gbcc_audio.device = SDL_OpenAudioDevice(NULL, 0, &want, &gbcc_audio.audiospec, 0);
 	if (gbcc_audio.device == 0) {
@@ -75,8 +91,8 @@ void gbcc_audio_update(struct gbc *gbc)
 	uint64_t clocks = gbcc_audio.cur_clock - gbcc_audio.old_clock;
 	if (clocks > SYNC_CLOCKS) {
 		gbcc_audio.old_clock = gbcc_audio.cur_clock;
-		uint64_t nsamples = 2*((double)clocks * GBC_CLOCK_PERIOD / 1e9) * SAMPLE_RATE;
-		if (nsamples > BUFSIZE / 2) {
+		uint64_t nsamples = ((double)clocks * GBC_CLOCK_PERIOD / 1e9) * SAMPLE_RATE;
+		if (nsamples > BUFSIZE / sizeof(Sint16)) {
 			return;
 		}
 		ch1_update(gbc, nsamples);
@@ -84,84 +100,87 @@ void gbcc_audio_update(struct gbc *gbc)
 		ch3_update(gbc, nsamples);
 		ch4_update(gbc, nsamples);
 		for (size_t i = 0; i < nsamples; i++) {
-			gbcc_audio.mix_buffer[i] = gbcc_audio.ch1_buffer[i]
-				+ gbcc_audio.ch2_buffer[i]
-				+ gbcc_audio.ch3_buffer[i]
-				+ gbcc_audio.ch4_buffer[i];
+			gbcc_audio.mix_buffer[i] = gbcc_audio.ch1.buffer[i]
+				+ gbcc_audio.ch2.buffer[i]
+				+ gbcc_audio.ch3.buffer[i]
+				+ gbcc_audio.ch4.buffer[i];
 		}
-		SDL_QueueAudio(gbcc_audio.device, gbcc_audio.mix_buffer, nsamples);
-		timespec_get(&gbcc_audio.cur_time, TIME_UTC);
-		struct timespec diff;
-		time_diff(&gbcc_audio.cur_time, &gbcc_audio.old_time, &diff);
-		if (diff.tv_nsec < SYNC_PERIOD) {
-			diff.tv_nsec = SYNC_PERIOD - diff.tv_nsec;
-			nanosleep(&diff, NULL);
+		int queued_samples = SDL_GetQueuedAudioSize(gbcc_audio.device);
+		SDL_QueueAudio(gbcc_audio.device, gbcc_audio.mix_buffer, nsamples * sizeof(Sint16));
+		uint64_t nsec = (double)queued_samples * 1e9 / SAMPLE_RATE;
+		if (nsec > LAG) {
+			struct timespec diff = {.tv_sec = 0, .tv_nsec = nsec - LAG};
+		//	nanosleep(&diff, &gbcc_audio.rem_time);
 		}
-		gbcc_audio.old_time = gbcc_audio.cur_time;
+		//printf("%d\n", SDL_GetQueuedAudioSize(gbcc_audio.device));
 	}
 }
 
 void ch1_update(struct gbc *gbc, uint64_t nsamples)
 {
-	static uint64_t sample = 0;
-	static uint16_t old_freq = 0;
-	static FILE *file = NULL;
-	if (file == NULL) {
-		file = fopen("sound.wav", "wbe");
-	}
+	uint64_t sample = gbcc_audio.ch1.sample;
 	uint8_t volume = (gbcc_memory_read(gbc, NR12, true) & 0xF0u) >> 4u;
-	uint16_t freq = gbcc_memory_read(gbc, NR13, true);
-	freq |= (gbcc_memory_read(gbc, NR14, true) & 0x03u) << 8u;
-	freq &= ~3;
-	if (freq != old_freq) {
-		sample = 0;
-		old_freq = freq;
+	uint64_t freq = gbcc_memory_read(gbc, NR13, true);
+	freq |= (gbcc_memory_read(gbc, NR14, true) & 0x07u) << 8u;
+	freq = 131072/(2048-freq);
+	uint8_t nr14 = gbcc_memory_read(gbc, NR24, true);
+	if (nr14 & bit(7)) {
+		gbcc_audio.ch1.counter = gbcc_memory_read(gbc, NR11, true) & 0x1Fu;
+		gbcc_memory_clear_bit(gbc, NR14, 7, true);
 	}
-	printf("%u:\t%lu\n", freq, sample);
 	for (size_t i = 0; i < nsamples; i++, sample++) {
+		if (nr14 & bit(6)) {
+			if (gbcc_audio.ch1.counter == 0) {
+				gbcc_audio.ch1.buffer[i] = 0;
+				continue;
+			}
+			gbcc_audio.ch1.counter--;
+		}
 		unsigned int time = lround(2 * sample * (double)freq / SAMPLE_RATE);
-		gbcc_audio.ch1_buffer[i] = (Sint16)(volume * BASE_AMPLITUDE * (time & 1u));
+		gbcc_audio.ch1.buffer[i] = (Sint16)(volume * BASE_AMPLITUDE * (time & 1u));
 	}
-	//fwrite(gbcc_audio.ch2_buffer, 2, nsamples, file);
+	gbcc_audio.ch1.sample = sample;
 }
 
 void ch2_update(struct gbc *gbc, uint64_t nsamples)
 {
-	static uint64_t sample = 0;
-	static uint16_t old_freq = 0;
-	static FILE *file = NULL;
-	if (file == NULL) {
-		file = fopen("sound.wav", "wbe");
-	}
+	uint64_t sample = gbcc_audio.ch2.sample;
 	uint8_t volume = (gbcc_memory_read(gbc, NR22, true) & 0xF0u) >> 4u;
-	uint16_t freq = gbcc_memory_read(gbc, NR23, true);
-	freq |= (gbcc_memory_read(gbc, NR24, true) & 0x03u) << 8u;
-	freq &= ~3;
-	if (freq != old_freq) {
-		sample = 0;
-		old_freq = freq;
+	uint64_t freq = gbcc_memory_read(gbc, NR23, true);
+	freq |= (gbcc_memory_read(gbc, NR24, true) & 0x07u) << 8u;
+	freq = 131072/(2048-freq);
+	uint8_t nr24 = gbcc_memory_read(gbc, NR24, true);
+	if (nr24 & bit(7)) {
+		gbcc_audio.ch2.counter = gbcc_memory_read(gbc, NR21, true) & 0x1Fu;
+		gbcc_memory_clear_bit(gbc, NR24, 7, true);
 	}
-	printf("%u:\t%lu\n", freq, sample);
 	for (size_t i = 0; i < nsamples; i++, sample++) {
+		if (nr24 & bit(6)) {
+			if (gbcc_audio.ch2.counter == 0) {
+				gbcc_audio.ch2.buffer[i] = 0;
+				continue;
+			}
+			gbcc_audio.ch2.counter--;
+		}
 		unsigned int time = lround(2 * sample * (double)freq / SAMPLE_RATE);
-		gbcc_audio.ch2_buffer[i] = (Sint16)(volume * BASE_AMPLITUDE * (time & 1u));
+		gbcc_audio.ch2.buffer[i] = (Sint16)(volume * BASE_AMPLITUDE * (time & 1u));
 	}
-	//fwrite(gbcc_audio.ch2_buffer, 2, nsamples, file);
+	gbcc_audio.ch2.sample = sample;
 }
 
 void ch3_update(struct gbc *gbc, uint64_t nsamples)
 {
-	static uint64_t sample = 0;
+	uint64_t sample = 0;
 	for (size_t i = 0; i < nsamples; i++, sample++) {
-		gbcc_audio.ch3_buffer[i] = 0;
+		gbcc_audio.ch3.buffer[i] = 0;
 	}
 }
 
 void ch4_update(struct gbc *gbc, uint64_t nsamples)
 {
-	static uint64_t sample = 0;
+	uint64_t sample = 0;
 	for (size_t i = 0; i < nsamples; i++, sample++) {
-		gbcc_audio.ch4_buffer[i] = 0;
+		gbcc_audio.ch4.buffer[i] = 0;
 	}
 }
 
