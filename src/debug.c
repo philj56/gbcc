43f95bb1e8e46dccc910ead49dcd822583bc159c
Phil Jones <philj56@gmail.com>
2020-01-11 03:31:44 +0000

    Stub gbcc_log_append_debug when not in debug mode.

--- src/debug.c
+++ src/debug.c
@@ -319,6 +319,9 @@ void gbcc_log_append_warning(const char *const fmt, ...)
 
 void gbcc_log_append_debug(const char *const fmt, ...)
 {
+#ifndef DEBUG
+	return;
+#endif
 	va_list args;
 	va_start(args, fmt);
 	vprintf(fmt, args);
