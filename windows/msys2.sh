db0d7762f1ece08e285b660b1b424d3177341a6e
Phil Jones <philj56@gmail.com>
2020-01-20 00:11:02 +0000

    Let's try that again, all is not a number.

--- windows/msys2.sh
+++ windows/msys2.sh
@@ -40,7 +40,7 @@ while true; do
 done
 
 if $INSTALL_DEPS; then
-	echo all | pacman -S --needed\
+	echo | pacman -S --needed\
 		mingw-w64-x86_64-toolchain\
 		mingw-w64-x86_64-clang\
 		mingw-w64-x86_64-lld\
