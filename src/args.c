d0aa8df87218b8be9a513e61fae47c269334ee0b
Phil Jones <philj56@gmail.com>
2020-01-28 19:27:16 +0000

    Add licence header to source files.

--- src/args.c
+++ src/args.c
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2017-2020 Philip Jones
+ *
+ * Licensed under the MIT License.
+ * See either the LICENSE file, or:
+ *
+ * https://opensource.org/licenses/MIT
+ *
+ */
+
 #include "args.h"
 #include "config.h"
 #include "debug.h"
