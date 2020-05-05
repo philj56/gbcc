c1e359b96985bace95f31d3517d7fa9a21bd9599
Phil Jones <philj56@gmail.com>
2020-05-05 21:27:32 +0100

    Print register state when "LD D,D" is encountered.

--- src/ops.c
+++ src/ops.c
@@ -353,6 +353,10 @@ void DI(struct gbcc_core *gbc)
 
 void LD_REG_REG(struct gbcc_core *gbc)
 {
+	if (gbc->cpu.opcode == 0x52) {
+		/* Use ld d,d as a debug statement */
+		gbcc_print_registers(gbc);
+	}
 	WRITE_OPERAND_DIV(gbc, 0x40u, READ_OPERAND_MOD(gbc));
 	done(&gbc->cpu);
 }
