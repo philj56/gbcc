65a03121f23cc2186088c33959550622ef774a8a
Phil Jones <philj56@gmail.com>
2020-09-07 11:15:32 +0100

    Add null audio platform & move headless.c

---
+++ src/audio_platform/null.c
@@ -0,0 +1,6 @@
+#include "../gbcc.h"
+
+void gbcc_audio_play_wav(const char *filename) {};
+void gbcc_audio_platform_initialise(struct gbcc *gbc) {};
+void gbcc_audio_platform_destroy(struct gbcc *gbc) {};
+void gbcc_audio_platform_queue_buffer(struct gbcc *gbc) {};
